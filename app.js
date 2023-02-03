639fbc311b6fdaa6b23c76b52a975931b4e5d995
Radwa Hamed <radwa.hamed@ibm.com>
2023-02-03 17:09:04 +0200

    push the code

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ let port = 3000;
 
 app.get("/", (req, res) => res.send("hello DTRS"));
 
-app.listen(port, () => console.log("Example app listening at http://localhost:3000"));
+app.listen(port, () => console.log('Example app listening at http://localhost:3000'));
